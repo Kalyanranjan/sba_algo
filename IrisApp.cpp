a9596deb926e8fff5a00da85ef9e6f658bcf90fb
Kalyan Parajuli <kalyanranjanp@gmail.com>
2017-04-09 21:56:17 -0400

    Initial Commit: Configuration initiated

---
+++ IrisApp.cpp
@@ -0,0 +1,32 @@
+
+#include <fstream>
+#include <iterator>
+#include <stdexcept>
+
+#include "./OsiManager.h"
+#include "./OsiStringUtils.h"
+using namespace std;
+using namespace osiris;
+
+int main(int argc, char const *argv[]) {
+  // Create black empty images
+  // cv::Mat image = cv::Mat::zeros( 400, 400, CV_8UC3 );
+  //
+  // // Draw a line
+  // cv::line( image, cv::Point( 15, 20 ), cv::Point( 70, 50), cv::Scalar( 110, 220, 0 ),  2, 8 );
+  // cv::imshow("Image",image);
+  //
+  // cv::waitKey( 0 );
+  try
+  {
+      OsiManager osi ;
+      osi.loadConfiguration(argv[1]) ;
+      osi.showConfiguration();
+      osi.run();
+  }
+  catch ( std::exception & e )
+  {
+      std::cout << e.what() << std::endl ;
+  }
+  return 0;
+}
---
+++ makefile
@@ -0,0 +1,1 @@
+	g++ IrisApp.cpp OsiManager.cpp OsiEye.cpp OsiProcessings.cpp OsiCircle.cpp -o osiris `pkg-config opencv --cflags --libs`
